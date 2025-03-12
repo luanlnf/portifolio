b3781d84971fe67ec070d96c600c8fbd26fc0a3c
LUAN FERREIRA <130109744+luanlnf@users.noreply.github.com>
2025-03-12 14:54:12 -0300

    fix: "sessão sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,11 @@ const Sobre = () => (
       debitis quos cum nulla quae tenetur voluptatum error dicta, impedit
       voluptas!
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=luanlnf&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=luanlnf&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 export default Sobre
