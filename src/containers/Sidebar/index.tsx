5fb1f9d6bcfe832e9a7318db96515bd46464de4a
LUAN FERREIRA <130109744+luanlnf@users.noreply.github.com>
2025-03-12 15:44:47 -0300

    fix: estilizando sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/luanlnf.png" />
-    <Titulo fontSize={20}>Luan Ferreira</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Luan Ferreira</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        luanlnf
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Menino de Programa
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
