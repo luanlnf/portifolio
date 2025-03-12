593d167239e78d5c7709278deb8157c6de9586f2
LUAN FERREIRA <130109744+luanlnf@users.noreply.github.com>
2025-03-12 13:39:24 -0300

    Construindo a estrutura

---
+++ src/containers/Projetos/index.tsx
@@ -0,0 +1,8 @@
+import Titulo from '../../components/Titulo'
+
+const Projetos = () => (
+  <section>
+    <Titulo>Projetos</Titulo>
+  </section>
+)
+export default Projetos
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre Mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere quisquam
+      velit cupiditate totam sequi soluta. Ducimus, voluptates! Iusto sint
+      debitis quos cum nulla quae tenetur voluptatum error dicta, impedit
+      voluptas!
+    </p>
+  </section>
+)
+export default Sobre
